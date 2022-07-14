1acab9d5ebd78dd686809af1bc489140ecffc341
dev-igss <100218781+dev-igss@users.noreply.github.com>
2022-07-14 13:05:06 -0600

    14072022
    correccion vista impresion por lote

--- code/code-dev/resources/views/admin/diet_request/print_lote.blade.php
+++ code/code-dev/resources/views/admin/diet_request/print_lote.blade.php
@@ -279,7 +279,7 @@
 
 
 
-                                        @endif
+
                                     @endif
                                 @endif
                             @endforeach
